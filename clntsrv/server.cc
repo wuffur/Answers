bca6fdec57ec70b153b3d495ca4fb533c251eb0f
Ivan <wuffur@gmail.com>
2013-07-25 02:28:55 +0400

    Update server.cc

--- clntsrv/server.cc
+++ clntsrv/server.cc
@@ -22,10 +22,10 @@ class Encryption_server : public L4::Server_object
 {
 public:
   int dispatch(l4_umword_t obj, L4::Ipc::Iostream &ios);
+ int encrypt(char *buffer, unsigned long buffer_size);
+ int decrypt(char *buffer, unsigned long buffer_size);
 private:
- static int encrypt(char *buffer, unsigned long buffer_size);
- static int decrypt(char *buffer, unsigned long buffer_size);
- static char backverse(const char c);
+ char backverse(const char c);
 
 };
 
@@ -46,39 +46,50 @@ Encryption_server::dispatch(l4_umword_t, L4::Ipc::Iostream &ios)
   switch (opcode)
     {
     case Opcode::func_encrypt:
-      ios >> L4::Ipc::buf_cp_in(buffer, buffer_size);
+      ios >> L4::Ipc::Buf_in<char>(buffer, buffer_size);
+      if(buffer_size > (L4_UTCB_GENERIC_DATA_SIZE-2)*sizeof(l4_umword_t))
+	    return  -L4_EBADPROTO;	
       retrun = encrypt(buffer, buffer_size);
       if(retrun != L4_EOK)
-     return retrun;
-      ios << L4::Ipc::buf_cp_out(buffer, buffer_size);
+	   return retrun;
+      ios << L4::Ipc::Buf_cp_out<char>(buffer, buffer_size);
       return L4_EOK;
     case Opcode::func_decrypt:
-      ios >> L4::Ipc::buf_cp_in(buffer, buffer_size);
+      ios >> L4::Ipc::Buf_in<char>(buffer, buffer_size);
+      if(buffer_size > (L4_UTCB_GENERIC_DATA_SIZE-2)*sizeof(l4_umword_t))
+	    return  -L4_EBADPROTO;	
       retrun = decrypt(buffer, buffer_size);
       if(retrun != L4_EOK)
 	   return retrun;
-      ios << L4::Ipc::buf_cp_out(buffer, buffer_size);
+      ios << L4::Ipc::Buf_cp_out<char>(buffer, buffer_size);
       return L4_EOK;
     default:
       return -L4_ENOSYS;
     };
 }
-	int Encryption_server::encrypt(char *buffer, unsigned long buffer_size){
+int Encryption_server::encrypt(char *buffer, unsigned long buffer_size){
 	if(buffer == NULL && buffer_size == 0)
 		return -L4_EINVAL;
-	
+	       char temp[buffer_size];	
 	for(unsigned long i=0; i<buffer_size; i++)
-		buffer[buffer_size-i-1] = backverse(buffer[i]);
+       		temp[i]	= buffer[i];
+
+	for(unsigned long i=0; i<buffer_size; i++)
+		buffer[buffer_size-i-1] = backverse('a');//backverse(temp[i]);
+	return 0;
 }
 
-	int Encryption_server::decrypt(char *buffer, unsigned long buffer_size){
+int Encryption_server::decrypt(char *buffer, unsigned long buffer_size){
 	if(buffer == NULL && buffer_size == 0)
 		return -L4_EINVAL;
-	
+       char temp[buffer_size];	
 	for(unsigned long i=0; i<buffer_size; i++)
-		buffer[buffer_size-i-1] = backverse(buffer[i]);
-}
+       		temp[i]	= buffer[i];
 
+	for(unsigned long i=0; i<buffer_size; i++)
+		buffer[buffer_size-i-1] = backverse('a');//backverse(temp[i]);
+	return 0;
+}
  char Encryption_server::backverse(const char c){
       if (islower(c))
 	return ('z'-c+'a');
@@ -87,6 +98,29 @@ Encryption_server::dispatch(l4_umword_t, L4::Ipc::Iostream &ios)
       else
 	return c;
 	}
+
+int
+main()
+{
+  static Encryption_server encr;
+
+
+
+  // Register calculation server
+  if (!server.registry()->register_obj(&encr, "crypt_server").is_valid())
+    {
+      printf("Something not let me doing server, is there 'encryption_server' in the caps table?\n");
+      return 1;
+    }
+
+  printf("Welcome to dat Encryption server!\n"
+         "I am wonderous magnificient part of program, doing magic.\n");
+
+  // Wait for client requests
+  server.loop();
+
+  return 0;
+}
 
 int
 main()
