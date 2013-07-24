13eb0e981df202c2234e3b3eb2f0b728d3e7e6e0
Ivan <wuffur@gmail.com>
2013-07-25 00:08:40 +0400

    Update client.cc

--- clntsrv/client.cc
+++ clntsrv/client.cc
@@ -1,7 +1,7 @@
 /*
  * (c) 2008-2009 Adam Lackorzynski <adam@os.inf.tu-dresden.de>,
  *               Alexander Warg <warg@os.inf.tu-dresden.de>
- *     economic rights: Technische UniversitГ¤t Dresden (Germany)
+ *     economic rights: Technische Universität Dresden (Germany)
  *
  * This file is part of TUD:OS and distributed under the terms of the
  * GNU General Public License 2.
@@ -12,56 +12,67 @@
 #include <l4/re/env>
 #include <l4/re/util/cap_alloc>
 #include <l4/cxx/ipc_stream>
+#include <string.h>
 #include <stdio.h>
+
 #include "shared.h"
+
 static int
-func_neg_call(L4::Cap<void> const &server, l4_uint32_t *result,
-              l4_uint32_t val)
+func_encrypt_call(L4::Cap<void> const &server, char *result,
+              char *message, unsigned long size)
 {
   L4::Ipc::Iostream s(l4_utcb());
-  s << l4_umword_t(Opcode::func_neg) << val;
-  int r = l4_error(s.call(server.cap(), Protocol::Calc));
-  if (r)
-    return r; // failure
-  s >> *result;
+  s << l4_umword_t(Opcode::func_encrypt) << L4::Ipc::buf_cp_out(message, size);
+  int retrun = l4_error(s.call(server.cap(), Protocol::Encryption));
+  if (retrun)
+    return retrun; // failure
+  //result = new char(sizeof(s));
+  s >> L4::Ipc::buf_cp_in(result, size);
   return 0; // ok
 }
+
 static int
-func_sub_call(L4::Cap<void> const &server, l4_uint32_t *result,
-              l4_uint32_t val1, l4_uint32_t val2)
+func_decrypt_call(L4::Cap<void> const &server, char *result, 
+             char *message, unsigned long size)
 {
   L4::Ipc::Iostream s(l4_utcb());
-  s << l4_umword_t(Opcode::func_sub) << val1 << val2;
-  int r = l4_error(s.call(server.cap(), Protocol::Calc));
-  if (r)
-    return r; // failure
-  s >> *result;
+  s << l4_umword_t(Opcode::func_decrypt) << L4::Ipc::buf_cp_out(message, size);
+  int retrun = l4_error(s.call(server.cap(), Protocol::Encryption));
+  if (retrun)
+    return retrun; // failure
+  s >> L4::Ipc::buf_cp_in(result, size);
   return 0; // ok
 }
+
 int
 main()
 {
-  L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("calc_server");
+//  sleep(1);
+  L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("crypt_server");
+ 
+
   if (!server.is_valid())
     {
-      printf("Could not get server capability!\n");
+      fprintf(stderr ,"Could not get server capability!\n");
       return 1;
     }
-  l4_uint32_t val1 = 8;
-  l4_uint32_t val2 = 5;
-  printf("Asking for %d - %d\n", val1, val2);
-  if (func_sub_call(server, &val1, val1, val2))
+
+  puts("this is da encryptin' bro!\n");
+  char str[14]="Hello world!";
+  char encstr[14],outstr[14];
+  if (func_encrypt_call(server, str, encstr, sizeof(str)))
     {
-      printf("Error talking to server\n");
+      fprintf(stderr, "Error talking to server\n");
       return 1;
     }
-  printf("Result of substract call: %d\n", val1);
-  printf("Asking for -%d\n", val1);
-  if (func_neg_call(server, &val1, val1))
+  printf("Result of da encryptin': %s\n", encstr);
+  puts("this is da decryptin' bro!\n");
+  if (func_decrypt_call(server, encstr, outstr, sizeof(encstr)))
     {
-      printf("Error talking to server\n");
+      fprintf(stderr, "Error talking to server\n");
       return 1;
     }
-  printf("Result of negate call: %d\n", val1);
+  printf("Result of da decryptin' call: %s\n", outstr);
+
   return 0;
 }
