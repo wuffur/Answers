39f597a583fa529c987ca6345d301e41907fb652
Ivan <wuffur@gmail.com>
2013-07-25 02:27:06 +0400

    Update client.cc

--- clntsrv/client.cc
+++ clntsrv/client.cc
@@ -7,6 +7,10 @@
  * GNU General Public License 2.
  * Please see the COPYING-GPL-2 file for details.
  */
+#include <l4/re/env>
+#include <l4/sys/ipc.h>
+#include <l4/sys/vcon>
+
 #include <l4/sys/err.h>
 #include <l4/sys/types.h>
 #include <l4/re/env>
@@ -22,12 +26,12 @@ func_encrypt_call(L4::Cap<void> const &server, char *result,
               char *message, unsigned long size)
 {
   L4::Ipc::Iostream s(l4_utcb());
-  s << l4_umword_t(Opcode::func_encrypt) << L4::Ipc::buf_cp_out(message, size);
+  s << l4_umword_t(Opcode::func_encrypt) << L4::Ipc::Buf_cp_out<char>(message, size);
   int retrun = l4_error(s.call(server.cap(), Protocol::Encryption));
   if (retrun)
     return retrun; // failure
   //result = new char(sizeof(s));
-  s >> L4::Ipc::buf_cp_in(result, size);
+  s >> L4::Ipc::Buf_cp_in<char>(result, size);
   return 0; // ok
 }
 
@@ -36,18 +40,17 @@ func_decrypt_call(L4::Cap<void> const &server, char *result,
              char *message, unsigned long size)
 {
   L4::Ipc::Iostream s(l4_utcb());
-  s << l4_umword_t(Opcode::func_decrypt) << L4::Ipc::buf_cp_out(message, size);
+  s << l4_umword_t(Opcode::func_decrypt) << L4::Ipc::Buf_cp_out<char>(message, size);
   int retrun = l4_error(s.call(server.cap(), Protocol::Encryption));
   if (retrun)
     return retrun; // failure
-  s >> L4::Ipc::buf_cp_in(result, size);
+  s >> L4::Ipc::Buf_cp_in<char>(result, size);
   return 0; // ok
 }
 
 int
 main()
 {
-//  sleep(1);
   L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("crypt_server");
  
 
@@ -58,21 +61,34 @@ main()
     }
 
   puts("this is da encryptin' bro!\n");
-  char str[14]="Hello world!";
-  char encstr[14],outstr[14];
-  if (func_encrypt_call(server, str, encstr, sizeof(str)))
+  char str[13]="Hello world!";
+  char encstr[13],outstr[13];
+  for(long int i=0;i<13;i++)
+	printf("%c",str[i]);
+  printf("\n");	
+  int retrun = func_encrypt_call(server, str, encstr, 13*sizeof(char));
+  if (retrun !=L4_EOK)
     {
-      fprintf(stderr, "Error talking to server\n");
+      printf("Error talking to server\n");
       return 1;
     }
-  printf("Result of da encryptin': %s\n", encstr);
+  printf("Result of da encryptin':");
+  for(long int i=0;i<13;i++)
+	printf("%c",encstr[i]);
+  printf("\n");	
   puts("this is da decryptin' bro!\n");
-  if (func_decrypt_call(server, encstr, outstr, sizeof(encstr)))
+  retrun = func_decrypt_call(server, encstr, outstr, 13*sizeof(char));
+  if (retrun != L4_EOK)
     {
-      fprintf(stderr, "Error talking to server\n");
+      printf("Error talking to server\n");
       return 1;
     }
-  printf("Result of da decryptin' call: %s\n", outstr);
-
+  printf("Result of da decryptin' call:"); 
+  for(long int i=0;i<13;i++)
+	printf("%c",outstr[i]);
+  printf("\n");	
+//  delete str;
+//  delete encstr;
+//  delete outstr;
   return 0;
 }
